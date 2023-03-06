0f4b7cf281e6b8919803ddae9ff317df774a6e4f
SoNearSonar <sonearsonar@gmail.com>
2023-03-05 21:44:35 -0500

    Upgrade project to .NET 6.0
    .NET 5.0 lost support so an upgrade to .NET 6.0 LTS can help

--- Tests/WaifuSharp.Tests/WaifuSharpTests.cs
+++ Tests/WaifuSharp.Tests/WaifuSharpTests.cs
@@ -1,46 +1,43 @@
 using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WaifuSharp.Models.Enums;
-using Xunit;
 
-namespace WaifuSharp.Tests
+namespace WaifuSharp.Tests;
+
+[TestClass]
+public class WaifuSharpTests
 {
-    public class WaifuSharpTests
+    [TestMethod]
+    public void GetSfwImageAsync_CategoryGiven_ReturnImage()
     {
-        [Theory]
-        [MemberData(nameof(EnumValues), typeof(SfwCategory))]
-        public async Task GetSfwImageAsync_CategoryGiven_ReturnImage(SfwCategory category)
-        {
-            // Arrange
-            var client = new WaifuClient();
+        // Arrange
+        var client = new WaifuClient();
 
-            // Act
-            var result = await client.GetSfwImageAsync(category);
+        // Act
+        var result = client.GetSfwImageAsync(SfwCategory.Neko).Result;
 
-            // Assert
-            result.ImageUrl.Should().NotBeNull();
-        }
+        // Assert
+        result.ImageUrl.Should().NotBeNull();
+    }
 
-        [Theory]
-        [MemberData(nameof(EnumValues), typeof(NsfwCategory))]
-        public async Task GetNsfwImageAsync_CategoryGiven_ReturnImage(NsfwCategory category)
-        {
-            // Arrange
-            var client = new WaifuClient();
+    [TestMethod]
+    public void GetNsfwImageAsync_CategoryGiven_ReturnImage()
+    {
+        // Arrange
+        var client = new WaifuClient();
 
-            // Act
-            var result = await client.GetNsfwImageAsync(category);
+        // Act
+        var result = client.GetNsfwImageAsync(NsfwCategory.Neko).Result;
 
-            // Assert
-            result.ImageUrl.Should().NotBeNull();
-        }
+        // Assert
+        result.ImageUrl.Should().NotBeNull();
+    }
 
 
-        public static IEnumerable<object[]> EnumValues(object enumToTest)
-        {
-            foreach (var thing in Enum.GetValues(enumToTest as Type ?? typeof(SfwCategory))) yield return new[] {thing};
-        }
+    public static IEnumerable<object[]> EnumValues(object enumToTest)
+    {
+        foreach (var thing in Enum.GetValues(enumToTest as Type ?? typeof(SfwCategory))) yield return new[] { thing };
     }
 }
